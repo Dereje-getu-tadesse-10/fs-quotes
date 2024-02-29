4da8e1bd87179c675a6e124e0ae8a76666e73277
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-02-29 21:31:53 +0100

    update: add cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://www.focusedshares.com'],
+    methods: ['GET'],
+    maxAge: 3600,
+  });
   await app.listen(3000);
 }
 bootstrap();
