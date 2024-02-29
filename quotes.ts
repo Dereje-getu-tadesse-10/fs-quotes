3a384c26db106e97500f84ceebc41738ea7fd560
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-02-29 20:17:25 +0100

    update: create quotes file and update app test

---
+++ quotes.ts
@@ -0,0 +1,75 @@
+export const quotes = [
+  {
+    quote: "Don't sit too long; stand up and stretch regularly.",
+  },
+  {
+    quote: 'Drink water and stay hydrated.',
+  },
+  {
+    quote: 'Success is the best revenge.',
+  },
+  {
+    quote: 'Take short breaks to clear your mind.',
+  },
+  {
+    quote: 'A cluttered desk is a cluttered mind.',
+  },
+  {
+    quote:
+      'Early to bed and early to rise makes a person healthy, wealthy, and wise.',
+  },
+  {
+    quote:
+      'Physical fitness is not only one of the most important keys to a healthy body, it is the basis of dynamic and creative intellectual activity.',
+  },
+  {
+    quote: 'The best time for new beginnings is now.',
+  },
+  {
+    quote: 'One day or day one. You decide.',
+  },
+  {
+    quote: "Your limitation—it's only your imagination.",
+  },
+  {
+    quote: 'Push yourself, because no one else is going to do it for you.',
+  },
+  {
+    quote: 'Great things never come from comfort zones.',
+  },
+  {
+    quote: 'Dream it. Wish it. Do it.',
+  },
+  {
+    quote: 'Success doesn’t just find you. You have to go out and get it.',
+  },
+  {
+    quote:
+      'The harder you work for something, the greater you’ll feel when you achieve it.',
+  },
+  {
+    quote: 'Dream bigger. Do bigger.',
+  },
+  {
+    quote: 'Don’t stop when you’re tired. Stop when you’re done.',
+  },
+  {
+    quote: 'Wake up with determination. Go to bed with satisfaction.',
+  },
+  {
+    quote: 'Do something today that your future self will thank you for.',
+  },
+  {
+    quote: 'Little things make big days.',
+  },
+  {
+    quote: 'It’s going to be hard, but hard does not mean impossible.',
+  },
+  {
+    quote: 'Don’t wait for opportunity. Create it.',
+  },
+  {
+    quote:
+      'Sometimes we’re tested not to show our weaknesses, but to discover our strengths.',
+  },
+];
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  sayHello(): { app: string; date: string } {
+    return {
+      app: 'Focused shares quotes',
+      date: '29/02/2024',
+    };
   }
 }
